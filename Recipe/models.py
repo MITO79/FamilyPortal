ba1ed6edf9110c878c34adf272a9f2defc882c55
MITO79 <m_itou@lumica.co.jp>
2022-05-10 18:01:21 +0900

    master

--- Recipe/models.py
+++ Recipe/models.py
@@ -2,17 +2,27 @@ from django.db import models
 
 # 未migrate！！
 # Create your models here.
-class ta_menu(models.Model):
-    id = models.AutoField(primary_key=True)
-    menu = models.CharField(max_length=255,default='')
-    category_id = models.ForeignKey(ma_category,on_delete=models.PROTECT)
-
-    def __str__(self):
-        return str(self.id) + ':' + self.menu
-
 class ma_category(models.Model):
-    id = models.AutoField(primary_key=True)
-    category = models.CharField(max_length=255,default='')
+    category = models.CharField(max_length=255,default='',unique=True)
 
     def __str__(self):
-        return str(self.id) + ':' + self.category
+        return self.category
+
+class ta_menu(models.Model):
+    menu = models.CharField(max_length=255,default='')
+    category = models.ForeignKey(ma_category,on_delete=models.PROTECT)
+    url = models.URLField(null=False)
+
+    def __str__(self):
+        return self.menu
+
+class ta_recipe(models.Model):
+    menu = models.ForeignKey(ta_menu)
+    ingredient = models.CharField(max_length=255,default='')    #材料
+    unit = models.CharField(max_length=255,default='大さじ')
+    volume = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.menu
+
+
