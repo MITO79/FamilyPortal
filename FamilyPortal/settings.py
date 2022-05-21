9c514d31bd0ad4e2ddbaacc84668d10428649356
MITO79 <m_itou@lumica.co.jp>
2022-05-22 00:11:47 +0900

    master

--- FamilyPortal/settings.py
+++ FamilyPortal/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j(l1n2ah&tb_0o@czpjhugn&=%8xs4o35a#goop^o@#(mbcytr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['iouaaa.pythonanywhere.com']
 
 
 # Application definition
