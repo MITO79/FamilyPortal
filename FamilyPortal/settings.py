b0806aea9b3339b992b32752f8b9938054a2fa10
MITO79 <m_itou@lumica.co.jp>
2022-05-23 16:29:37 +0900

    master

--- FamilyPortal/settings.py
+++ FamilyPortal/settings.py
@@ -156,7 +156,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+if DEBUG == False:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
+else:
+    pass
+
 #STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
